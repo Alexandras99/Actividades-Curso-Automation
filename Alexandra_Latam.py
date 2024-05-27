80e35a541c4df93a74901fc87ce0a696dd7714c9
Alexandras99 <Alexandras99@gmail.com>
2024-05-27 19:38:33 -0300

    Deje solo el de buenas practicas

--- Alexandra_Latam.py
+++ Alexandra_Latam.py
@@ -2,69 +2,58 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
-import time
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 
 # Se accede a la página web 
 driver= webdriver.Chrome()
+
+driver.implicitly_wait(5)
+
 driver.get("https://www.google.com")
 
 # Maximiza la ventana
 driver.maximize_window()
 
-# Tiempo de espera
-time.sleep(5)
-
 # Se ingresa la palabra a buscar
-Cuadro_busqueda= driver.find_element(By.NAME, 'q')
-Cuadro_busqueda.send_keys("Latam" + Keys.ENTER)
-
-# Tiempo de espera
-time.sleep(5)
+cuadro_busqueda= WebDriverWait(driver,10).until(EC.presence_of_element_located((By.NAME, 'q'))) 
+cuadro_busqueda.clear()
+cuadro_busqueda.send_keys("Latam" + Keys.ENTER)
 
 # Se selecciona el resultado de búsqueda
-Resultado= driver.find_element(By.XPATH, "//h3[contains(text(),'Cotiza Vuelos, Seguros, Hoteles y Autos | LATAM en')]")
-Resultado.click()
-
-# Tiempo de espera
-time.sleep(5)
+resultado= WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.XPATH, "//h3[contains(text(),'Cotiza Vuelos, Seguros, Hoteles y Autos | LATAM en')]"))) 
+resultado.click()
 
 #Ingresar Origen
+origen= WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.XPATH, "//input[@id='txtInputOrigin_field']")))  
+origen.clear()
+origen.send_keys("Buenos Aires")
+origen= WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.XPATH, "//span[contains(text(), 'Todos los aeropuertos')]")))
+origen.click()
 
-Origen= driver.find_element(By.XPATH, "//input[@id='txtInputOrigin_field']")
-Origen.send_keys("Buenos Aires")
-Origen= driver.find_element(By.XPATH, "//span[contains(text(), 'Todos los aeropuertos')]")
-Origen.click()
 
 # Ingresar Destino
-Destino= driver.find_element(By.XPATH, "//input[@id='txtInputDestination_field']")
-Destino.send_keys("Italia")
-Destino= driver.find_element(By.XPATH, "//button[@id='btnItemAutoComplete_0']//span[@class='sc-inZACr hEOYcR'][normalize-space()='Todos los aeropuertos']")
-Destino.click()
+destino= WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.XPATH, "//input[@id='txtInputDestination_field']"))) 
+destino.clear()
+destino.send_keys("Italia")
+destino= WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.XPATH, "//button[@id='btnItemAutoComplete_0']//span[@class='sc-inZACr hEOYcR'][normalize-space()='Todos los aeropuertos']"))) 
+destino.click()
 
-# Tiempo de espera
-time.sleep(5)
 
 # Seleccionar la Fecha de Ida
-Fecha_ida= driver.find_element(By.XPATH, "//div[@class='sc-cIIdys jEBCnD']")
-Fecha_ida.click()
-Fecha_ida= driver.find_element(By.XPATH, "//td[@aria-label='Elija miércoles, 26 de junio de 2024 como su fecha de ida. Está disponible.']")
-Fecha_ida.click()
+fecha_ida= WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.XPATH, "//input[@id='departureDate']"))) 
+fecha_ida.click()
+fecha_ida= WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.XPATH, "//td[@aria-label='Elija miércoles, 26 de junio de 2024 como su fecha de ida. Está disponible.']//span[@class='sc-kWhykh gTPxFH'][normalize-space()='26']"))) 
+fecha_ida.click()
 
 # Seleccionar la Fecha de Vuelta
-Fecha_vuelta= driver.find_element(By.XPATH, "//div[@class='sc-cIIdys gABgRF']")
-Fecha_vuelta.click()
-Fecha_vuelta= driver.find_element(By.XPATH, "//td[@aria-label='Elija viernes, 26 de julio de 2024 como fecha de vuelta. Está disponible.']")
-Fecha_vuelta.click()
-
-
-# Tiempo de espera
-time.sleep(5)
+fecha_vuelta= WebDriverWait(driver,10).until(EC.element_to_be_clickable((By.XPATH, "//input[@id='arrivalDate']"))) 
+fecha_vuelta.click()
+fecha_vuelta= WebDriverWait(driver,10).until(EC.element_to_be_clickable((By.XPATH, "//td[@aria-label='Elija viernes, 26 de julio de 2024 como fecha de vuelta. Está disponible.']")))
+fecha_vuelta.click()
 
 # Seleccionar el botón Buscar
-Buscar= driver.find_element(By.ID, 'btnSearchCTA')
-Buscar.click()
-
-# Tiempo de espera
-time.sleep(10)
+buscar= WebDriverWait(driver,10).until(EC.element_to_be_clickable((By.ID, 'btnSearchCTA')))
+buscar.click()
 
 driver.quit()
